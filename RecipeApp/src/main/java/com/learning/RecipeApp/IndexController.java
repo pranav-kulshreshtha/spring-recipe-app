32557e37d07ed0afd4b9b8128729c401f6152a47
pranav.kulshreshtha_cs.aiml19 <pranav.kulshreshtha_cs.aiml19>
2021-06-14 19:00:13 +0530

    Added IndexController class, closes #1

---
+++ RecipeApp/src/main/java/com/learning/RecipeApp/IndexController.java
@@ -0,0 +1,15 @@
+package com.learning.RecipeApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping({"" , "/" , "/index" , "/index.html"})
+	public String showIndex(Model model) {
+		return "index";
+	}
+	
+}
