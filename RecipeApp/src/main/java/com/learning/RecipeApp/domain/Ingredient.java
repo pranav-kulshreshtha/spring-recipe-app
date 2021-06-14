a437dba05e314207eae427da37ed70d8d18d14cc
pranav.kulshreshtha_cs.aiml19 <pranav.kulshreshtha_cs.aiml19>
2021-06-14 19:52:44 +0530

    Getters and Setters for uom

--- RecipeApp/src/main/java/com/learning/RecipeApp/domain/Ingredient.java
+++ RecipeApp/src/main/java/com/learning/RecipeApp/domain/Ingredient.java
@@ -56,5 +56,13 @@ public class Ingredient {
 	public void setRecipe(Recipe recipe) {
 		this.recipe = recipe;
 	}
+
+	public UnitOfMeasure getUom() {
+		return uom;
+	}
+
+	public void setUom(UnitOfMeasure uom) {
+		this.uom = uom;
+	}
 	
 }
